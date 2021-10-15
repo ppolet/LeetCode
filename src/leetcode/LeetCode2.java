ca9de32b06ebce5a700cd9524849ef4b48ddb153
pm.poletaev <pm.poletaev@pc-so-it-9.sintez-oka.ru>
2021-10-15 13:56:44 +0300

    Sol 31

---
+++ src/leetcode/LeetCode2.java
@@ -0,0 +1,61 @@
+
+package leetcode;
+
+import java.util.Arrays;
+
+public class LeetCode2 {
+}
+
+//https://leetcode.com/problems/search-insert-position/
+//35. Search Insert Position
+//Easy
+//Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+//You must write an algorithm with O(log n) runtime complexity.
+//
+//Example 1:
+//Input: nums = [1,3,5,6], target = 5
+//Output: 2
+//
+//Example 2:
+//Input: nums = [1,3,5,6], target = 2
+//Output: 1
+//
+//Example 3:
+//Input: nums = [1,3,5,6], target = 7
+//Output: 4
+//
+//Example 4:
+//Input: nums = [1,3,5,6], target = 0
+//Output: 0
+//
+//Example 5:
+//Input: nums = [1], target = 0
+//Output: 0
+//
+//Constraints:
+//
+//    1 <= nums.length <= 104
+//    -104 <= nums[i] <= 104
+//    nums contains distinct values sorted in ascending order.
+//    -104 <= target <= 104
+
+
+class Solution31 {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            int midVal = nums[mid];
+
+            if (midVal < target)
+                low = mid + 1;
+            else if (midVal > target)
+                high = mid - 1;
+            else
+                return mid; // key found
+        }
+        return low;  // key not found.
+    }
+}
